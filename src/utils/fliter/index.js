1b97b26a2bdff89b4fa1339b4581449fc64c2730
eightfeet <bajiao000000@163.com>
2017-03-05 11:26:53 +0800

    fix

--- src/utils/fliter/index.js
+++ src/utils/fliter/index.js
@@ -7,41 +7,41 @@
  * @end {Number}
  */
 export function FStringPrivacy(data, start, end) {
-    const S = parseInt(start) || 1;
-    const E = parseInt(end) || 0;
+	const S = parseInt(start, 0) || 1;
+	const E = parseInt(end, 0) || 0;
 
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
 
-    Str
+	Str
         ? Str = Str.toString()
         : null;
-    if (!Str) {
-        return;
-    }
-    let N;
-    let HideSTR = '';
-    let result;
-    if (Str.length > (S + E)) {
-        N = Str.length - (S + E);
-        for (let i = 0; i < N; i++) {
-            HideSTR = HideSTR + '*';
-        }
-        result = `${Str.slice(0, S)}${HideSTR}${Str.slice((Str.length - E), Str.length)}`;
-    } else {
-        N = Str.length - S;
-        for (let i = 0; i < N; i++) {
-            HideSTR = HideSTR + '*';
-        }
-        result = `${Str.slice(0, S)}${HideSTR}`;
-    }
-    return result;
-};
+	if (!Str) {
+		return;
+	}
+	let N;
+	let HideSTR = '';
+	let result;
+	if (Str.length > (S + E)) {
+		N = Str.length - (S + E);
+		for (let i = 0; i < N; i++) {
+			HideSTR = HideSTR + '*';
+		}
+		result = `${Str.slice(0, S)}${HideSTR}${Str.slice((Str.length - E), Str.length)}`;
+	} else {
+		N = Str.length - S;
+		for (let i = 0; i < N; i++) {
+			HideSTR = HideSTR + '*';
+		}
+		result = `${Str.slice(0, S)}${HideSTR}`;
+	}
+	return result;
+}
 
 /**
  * 个位数前加零
@@ -50,7 +50,7 @@ export function FStringPrivacy(data, start, end) {
  */
 
 function zerofill(val) {
-    return val >= 10 ? val : '0' + val
+	return val >= 10 ? val : '0' + val;
 }
 
 /**
@@ -60,36 +60,36 @@ function zerofill(val) {
  * @return {String}
  */
 export function FTimeStamp(time, type, Zh) {
-    let text = Zh || '';
-    let date = new Date(parseInt(time));
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let second = date.getSeconds();
+	let text = Zh || '';
+	let date = new Date(parseInt(time, 0));
+	let year = date.getFullYear();
+	let month = date.getMonth() + 1;
+	let day = date.getDate();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let second = date.getSeconds();
 
-    switch (type) {
-        case 'MM-dd': // 01-05 (zh: 01月05日)
-            return text === 'Zh'
+	switch (type) {
+		case 'MM-dd': // 01-05 (zh: 01月05日)
+			return text === 'Zh'
                 ? `${zerofill(month)}月${zerofill(day)}日`
                 : `${zerofill(month)}-${zerofill(day)}`;
-        case 'HH:mm': // 11:12 (zh: 11时12分)
-            return text === 'Zh'
+		case 'HH:mm': // 11:12 (zh: 11时12分)
+			return text === 'Zh'
                 ? `${zerofill(hours)}时${zerofill(minutes)}分`
                 : `${zerofill(hours)}-${zerofill(minutes)}`;
-        case 'yyyy-MM-dd': // 2015-01-05 (zh: 2015年01月05日)
-            return text === 'Zh'
+		case 'yyyy-MM-dd': // 2015-01-05 (zh: 2015年01月05日)
+			return text === 'Zh'
                 ? `${year}年${zerofill(month)}月${zerofill(day)}日`
                 : `${year}-${zerofill(month)}-${zerofill(day)}`;
-        case 'yyyy-MM-dd HH:mm': // 2015-01-05 11:12 (zh: 2015年01月05日 11时12分)
-            return text === 'Zh'
+		case 'yyyy-MM-dd HH:mm': // 2015-01-05 11:12 (zh: 2015年01月05日 11时12分)
+			return text === 'Zh'
                 ? `${year}年${zerofill(month)}月${zerofill(day)}日  ${zerofill(hours)}时${zerofill(minutes)}分`
                 : `${year}-${zerofill(month)}-${zerofill(day)}  ${zerofill(hours)}:${zerofill(minutes)}`;
-        default: // 2015-01-05 11:12:13 (zh: 2015年01月05日 11时12分13秒)
+		default: // 2015-01-05 11:12:13 (zh: 2015年01月05日 11时12分13秒)
             // default yyyy-MM-dd HH:mm:ss
-            return text === 'Zh'
+			return text === 'Zh'
                 ? `${year}年${zerofill(month)}月${zerofill(day)}日  ${zerofill(hours)}时${zerofill(minutes)}分${zerofill(second)}秒`
                 : `${year}-${zerofill(month)}-${zerofill(day)}  ${zerofill(hours)}:${zerofill(minutes)}:${zerofill(second)}`;
-    }
-};
+	}
+}
--- src/utils/validate/index.js
+++ src/utils/validate/index.js
@@ -6,37 +6,37 @@
  * @strict {String}
  */
 export function VPhone(data, strict) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
 
-    Str
+	Str
         ? Str = Str.toString()
         : null;
 
-    const fixStrict = strict || null;
+	const fixStrict = strict || null;
 
-    if (!Str || Str.length !== 11) {
-        return ('请输入11位手机号码');
-    }
+	if (!Str || Str.length !== 11) {
+		return ('请输入11位手机号码');
+	}
 
-    if (!(/^[0-9]*$/.test(Str))) {
-        return ('手机号码格式不正确');
-    }
+	if (!(/^[0-9]*$/.test(Str))) {
+		return ('手机号码格式不正确');
+	}
 
-    if (fixStrict !== 'strict' && !(/^1\d{10}$/.test(Str))) {
-        return ('请输入以1开头的11位手机号码');
-    }
+	if (fixStrict !== 'strict' && !(/^1\d{10}$/.test(Str))) {
+		return ('请输入以1开头的11位手机号码');
+	}
 
-    if (fixStrict === 'strict' && !(/^1[3|4|5|7|8]\d{9}$/.test(Str))) {
-        return ('请输入正确手机号码');
-    }
+	if (fixStrict === 'strict' && !(/^1[3|4|5|7|8]\d{9}$/.test(Str))) {
+		return ('请输入正确手机号码');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -47,36 +47,36 @@ export function VPhone(data, strict) {
  * @Zh {String} = 'Zh' validate the chinese name
  */
 export function VName(data, Zh) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
-    const fixZh = Zh || null;
+	const fixZh = Zh || null;
 
-    if (!Str || Str.length < 1) {
-        return ('请输入您的姓名');
-    }
+	if (!Str || Str.length < 1) {
+		return ('请输入您的姓名');
+	}
 
-    if (fixZh !== 'Zh' && !(/^[\u4E00-\u9FA5A-Za-z0-9]+$/.test(Str))) {
-        return (`姓名请使用非特殊字符`);
-    }
+	if (fixZh !== 'Zh' && !(/^[\u4E00-\u9FA5A-Za-z0-9]+$/.test(Str))) {
+		return (`姓名请使用非特殊字符`);
+	}
 
-    if (fixZh === 'Zh' && Str.length < 2) {
-        return ('请输您的真实姓名');
-    }
+	if (fixZh === 'Zh' && Str.length < 2) {
+		return ('请输您的真实姓名');
+	}
 
-    if (fixZh === 'Zh' && !(/^[\u4e00-\u9fa5]+$/.test(Str))) {
-        return ('请输您的真实姓名');
-    }
+	if (fixZh === 'Zh' && !(/^[\u4e00-\u9fa5]+$/.test(Str))) {
+		return ('请输您的真实姓名');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -86,27 +86,27 @@ export function VName(data, Zh) {
  * @data {String} request
  */
 export function VEmail(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!Str || Str.length < 1) {
-        return ('请输入您的邮箱');
-    }
+	if (!Str || Str.length < 1) {
+		return ('请输入您的邮箱');
+	}
 
-    if (!(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/.test(Str))) {
-        return ('请输正确的邮箱地址');
-    }
+	if (!(/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/.test(Str))) {
+		return ('请输正确的邮箱地址');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -116,27 +116,27 @@ export function VEmail(data) {
  * @data {String} request
  */
 export function VSecurityCode(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!Str || Str.length !== 16) {
-        return ('请输入16位防伪码');
-    }
+	if (!Str || Str.length !== 16) {
+		return ('请输入16位防伪码');
+	}
 
-    if (!(/^[0-9]*$/.test(Str))) {
-        return ('您输入的防伪码格式不正确，请重新输入');
-    }
+	if (!(/^[0-9]*$/.test(Str))) {
+		return ('您输入的防伪码格式不正确，请重新输入');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -146,27 +146,27 @@ export function VSecurityCode(data) {
  * @data {String} request
  */
 export function VBarCode(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!Str || Str.length !== 13) {
-        return ('请输入13位产品条形码');
-    }
+	if (!Str || Str.length !== 13) {
+		return ('请输入13位产品条形码');
+	}
 
-    if (!(/^[0-9]*$/.test(data))) {
-        return ('您输入的产品条形码格式不正确，请重新输入');
-    }
+	if (!(/^[0-9]*$/.test(data))) {
+		return ('您输入的产品条形码格式不正确，请重新输入');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -177,32 +177,32 @@ export function VBarCode(data) {
  * @length {Number} default 4
  */
 export function VVerificationCode(data, length) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
-    const fixLength = length || 4;
+	const fixLength = length || 4;
 
-    if (length && isNaN(fixLength)) {
-        return '验证码验证时参数错误';
-    }
+	if (length && isNaN(fixLength)) {
+		return '验证码验证时参数错误';
+	}
 
-    if (!Str || Str.length !== fixLength) {
-        return (`请输入${fixLength}位验证码`);
-    }
+	if (!Str || Str.length !== fixLength) {
+		return (`请输入${fixLength}位验证码`);
+	}
 
-    if (!(/^[0-9]*$/.test(Str))) {
-        return ('您输入的验证码格式不正确，请重新输入');
-    }
+	if (!(/^[0-9]*$/.test(Str))) {
+		return ('您输入的验证码格式不正确，请重新输入');
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -213,28 +213,28 @@ export function VVerificationCode(data, length) {
  * @length {Number} request
  */
 export function VRequire(data, length) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
-    const fixLength = length || 1;
+	const fixLength = length || 1;
 
-    if (isNaN(fixLength) || !Str) {
-        return '必填项验证时参数错误';
-    }
+	if (isNaN(fixLength) || !Str) {
+		return '必填项验证时参数错误';
+	}
 
-    if (Str.length < fixLength) {
-        return true;
-    }
+	if (Str.length < fixLength) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -245,28 +245,28 @@ export function VRequire(data, length) {
  * @length {Number} request
  */
 export function VLimit(data, length) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
-    const fixLength = length || 20;
+	const fixLength = length || 20;
 
-    if (isNaN(fixLength) || !Str) {
-        return '限制字符串长度验证时参数错误';
-    }
+	if (isNaN(fixLength) || !Str) {
+		return '限制字符串长度验证时参数错误';
+	}
 
-    if (Str.length > fixLength) {
-        return true;
-    }
+	if (Str.length > fixLength) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -276,23 +276,23 @@ export function VLimit(data, length) {
  * @data {String} request
  */
 export function VNumber(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!(/^[0-9]*$/.test(Str))) {
-        return true;
-    }
+	if (!(/^[0-9]*$/.test(Str))) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -302,23 +302,23 @@ export function VNumber(data) {
  * @data {String} request
  */
 export function VChinese(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!(/^[\u4e00-\u9fa5]+$/.test(Str))) {
-        return true;
-    }
+	if (!(/^[\u4e00-\u9fa5]+$/.test(Str))) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -328,23 +328,23 @@ export function VChinese(data) {
  * @data {String} request
  */
 export function VEnglish(data) {
-    let Str;
+	let Str;
 
-    if (data !== 0) {
-        Str = data;
-    } else {
-        Str = '0';
-    }
-    
-    Str
+	if (data !== 0) {
+		Str = data;
+	} else {
+		Str = '0';
+	}
+
+	Str
         ? Str = Str.toString()
         : null;
 
-    if (!(/^[a-zA-Z]*$/.test(Str))) {
-        return true;
-    }
+	if (!(/^[a-zA-Z]*$/.test(Str))) {
+		return true;
+	}
 
-    return false;
+	return false;
 }
 
 /**
@@ -354,21 +354,21 @@ export function VEnglish(data) {
  * @arguments {String|Boolean} request
  */
 export default function validate() {
-    for (let i = 0; i < arguments.length; i++) {
-        const arg = arguments[i];
-        const argType = typeof arg;
+	for (let i = 0; i < arguments.length; i++) {
+		const arg = arguments[i];
+		const argType = typeof arg;
 
-        if (!arg) {
-            continue;
-        }
+		if (!arg) {
+			continue;
+		}
 
-        if (argType === 'string' || argType === 'boolean') {
-            return arg;
-        } else {
-            console.error(`Warning: Failed argument type expected "String|Boolean"  but type of" ${arg} is a ${typeof arg}`);
-            continue;
-        }
-    }
+		if (argType === 'string' || argType === 'boolean') {
+			return arg;
+		}
+		console.error(`Warning: Failed argument type expected "String|Boolean"  but type of" ${arg} is a ${typeof arg}`);
+		continue;
 
-    return false;
+	}
+
+	return false;
 }
