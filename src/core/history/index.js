2013dc832e25c9cf4f658f40742c8fec5e5c6912
xiaojining <bajiao000000@163.com>
2017-03-15 10:01:11 +0800

    fix

--- src/core/history/index.js
+++ src/core/history/index.js
@@ -1,4 +1,4 @@
 // public hash history
-import { createHashHistory } from 'history';
+import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory();
 export default history;
