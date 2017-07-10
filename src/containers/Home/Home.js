8942cf7502031604c21cc228c2f346ae6ddf1e32
eightfeet <bajiao000000@163.com>
2017-07-10 23:16:05 +0800

    fix

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,7 @@ import Modal from '~/components/Modal';
 import Loading from '~/components/Loading';
 import Spin from '~/components/Loading/Spin';
 import MotionPage from '~/components/MotionPage';
-import ScrollLoading from '~/components/ScrollLoading';
+import MyScroll from '~/components/MyScroll';
 import s from './style';
 import sl from './styleb';
 
@@ -35,13 +35,10 @@ class Home extends Component {
 		return (
 			<div className="pdt2 al-c center ww">
 				<div onClick={() => (history.push('./view'))}>测试</div>
-				<ScrollLoading
-					handlePage={this.handlePage}
-					scrollToTop
-				>	{
+				<MyScroll>	{
 						data.map((item) => (<p onClick={() => {window.alert(item)}} key={item} style={{height:'3rem'}}>list{item}</p>))
 					}
-				</ScrollLoading>
+				</MyScroll>
 			</div>
 		);
 	}
