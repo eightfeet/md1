349c5359267a77bc8ab33fd3058c39340862edf6
xiaojining <bajiao000000@163.com>
2017-07-10 14:49:59 +0800

    fix

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,7 @@ import Modal from '~/components/Modal';
 import Loading from '~/components/Loading';
 import Spin from '~/components/Loading/Spin';
 import MotionPage from '~/components/MotionPage';
-import ScrollLoading from '~/components/ScrollLoading';
+import ScrollList from '~/components/Scrolllist';
 import s from './style';
 import sl from './styleb';
 
@@ -15,7 +15,7 @@ class Home extends Component {
 			item: {
 				left: 10,
 				color: 'red',
-				rotate: 45,
+				rotate: 45
 			},
 			data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
 		};
@@ -35,13 +35,13 @@ class Home extends Component {
 		return (
 			<div className="pdt2 al-c center ww">
 				<div onClick={() => (history.push('./view'))}>测试</div>
-				<ScrollLoading
+				<ScrollList
 					handlePage={this.handlePage}
 					scrollToTop
 				>	{
-						data.map((item) => (<p onClick={() => {window.alert(item)}} key={item} style={{height:'3rem'}}>list{item}</p>))
+						data.map((item) => (<p onClick={() => {window.alert(item);}} key={item} style={{height:'3rem'}}>list{item}</p>))
 					}
-				</ScrollLoading>
+				</ScrollList>
 			</div>
 		);
 	}
