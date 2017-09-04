e421ed605af06caed7f77f866d8dc2332c23eb35
eightfeet <bajiao000000@163.com>
2017-09-05 00:59:02 +0800

    fix

--- src/data/index.js
+++ src/data/index.js
@@ -5,5 +5,6 @@ import models203_231 from './models203_231.json';
 import models232_270 from './models232_270.json';
 import models271_300 from './models271_300.json';
 import models301_384 from './models301_384.json';
+import models401_413 from './models401_413.json';
 
-export default [...models0_104, ...models105_176, ...models177_202, ...models203_231, ...models232_270, ...models271_300, ...models301_384];
+export default [...models0_104, ...models105_176, ...models177_202, ...models203_231, ...models232_270, ...models271_300, ...models301_384, ...models401_413];
